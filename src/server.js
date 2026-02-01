57b4ac4e705b0687dcbd47cf9d6a4fcb4121c572
Nguyen Van Ty <nguyenvanty011998@gmail.com>
2026-02-01 15:03:01 +0700

    refactor: server.js, env

--- src/server.js
+++ src/server.js
@@ -1,15 +1,55 @@
 import express from 'express'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World Nodejs Trungquandev</h1>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hi ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+  // thực hiện các tác vụ cleanup trước khi dừng server
+  exitHook(() => {
+    // eslint-disable-next-line no-console
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+    // eslint-disable-next-line no-console
+    console.log('5. Server is shut down!')
+  })
+}
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+// Chỉ khi Kết nối tới Database thành công thì mới Start Server Back-end lên.
+// Immediately-invoked / Anonymous Async Functions (IIFE)
+(async () => {
+  try {
+    // phải kết nối mongoDB thành công trước
+    // eslint-disable-next-line no-console
+    console.log('1. Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    // eslint-disable-next-line no-console
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+    // sau đó mới start server
+    START_SERVER()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    // Dừng chương trình Node.js ngay lập tức
+    // 0 = thoát bình thường, 1 = thoát do lỗi (nên dùng 1 khi có lỗi)
+    process.exit(0)
+  }
+})()
+
+// Cách 2: Dùng Promise chain (thay thế cho IIFE ở trên)
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
