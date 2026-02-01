eae3c05f8aa2a78233d72c174f161f54f4358c84
Nguyen Van Ty <nguyenvanty011998@gmail.com>
2026-02-01 16:56:39 +0700

    feat: update api v1 and api boards

---
+++ src/routes/v1/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from '~/routes/v1/board-routes'
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.', code: StatusCodes.OK })
+})
+
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
