1935802a00bbe7c3374fdc9217bfa1541e9979d6
Nguyen Van Ty <nguyenvanty011998@gmail.com>
2026-02-04 22:13:30 +0700

    perf: update script cross-env

--- src/middlewares/error-handling-middleware.js
+++ src/middlewares/error-handling-middleware.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
-// import { env } from '~/config/environment'
+import { env } from '~/config/environment'
 
 // Middleware xử lý lỗi tập trung trong ứng dụng Back-end NodeJS (ExpressJS)
 export const errorHandlingMiddleware = (err, req, res, next) => {
@@ -15,7 +15,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   // console.error(responseError)
 
   // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN)
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  if (env.BUILD_MODE !== 'development') delete responseError.stack
 
   // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...vv Hoặc có thể viết riêng
   // ...
