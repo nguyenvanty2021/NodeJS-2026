112fec6f54edcb26c3e3eb84ec330a43467c6ad0
Nguyen Van Ty <nguyenvanty011998@gmail.com>
2026-02-04 22:47:12 +0700

    feat: add services layer

--- src/controllers/board-controller.js
+++ src/controllers/board-controller.js
@@ -1,10 +1,12 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/board-service'
 
 const addNewBoard = async (req, res, next) => {
   try {
     // eslint-disable-next-line no-console
     console.log('req.body: ', req.body)
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API create new board' })
+    const createdBoard = await boardService.addNewBoard(req.body)
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     // next(error) sẽ chuyển lỗi này sang middleware xử lý lỗi tập trung trong file server.js
     next(error)
---
+++ src/services/board-service.js
@@ -0,0 +1,22 @@
+import { slugify } from '~/utils/formatters'
+
+const createNew = (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const newBoard = {
+      ...reqBody,
+      slug: slugify(reqBody.title)
+    }
+
+    // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+    // ...
+
+    // Làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án...vv
+    // Bắn email, notification về cho admin khi có 1 cái board mới được tạo...vv
+    return newBoard
+  } catch (error) { throw error }
+}
+
+export const boardService = {
+  createNew
+}
