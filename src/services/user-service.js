5eec2a6e94b9564f1f65732ebaeb6a5fc044ae6b
Nguyen Van Ty <nguyenvanty011998@gmail.com>
2026-02-09 22:34:47 +0700

    feat: add logic send email after register

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -4,6 +4,8 @@ import { StatusCodes } from 'http-status-codes'
 import bcryptjs from 'bcryptjs'
 import { v4 as uuidv4 } from 'uuid'
 import { pickData } from '~/utils/formatters'
+import { WEBSITE_DOMAIN } from '~/utils/constants'
+import { BrevoProvider } from '~/providers/brevo-provider'
 
 const register = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -30,6 +32,16 @@ const register = async (reqBody) => {
     const getNewUser = await userModel.findOneById(createdUser.insertedId)
 
     // Gửi email cho người dùng xác thực tài khoản (buổi sau...)
+    const verificationLink = `${WEBSITE_DOMAIN}/account/verification?email=${getNewUser.email}&token=${getNewUser.verifyToken}`
+    const customSubject = 'Trello MERN Stack Advanced: Please verify your email before using our services!'
+    const htmlContent = `
+      <h3>Here is your verification link:</h3>
+      <h3>${verificationLink}</h3>
+      <h3>Sincerely,<br/> - Company Name - </h3>
+    `
+
+    // gọi hàm gửi email
+    await BrevoProvider.sendEmail({ recipientEmail: getNewUser.email, customSubject, customHtmlContent: htmlContent })
 
     // return trả về dữ liệu cho phía Controller
     // bỏ password khỏi object getNewUser => không return password sau khi hash cho frontend
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 
+import { env } from '~/config/environment'
+
 // những domain được phép truy cập đến tài nguyên của server
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
@@ -9,3 +11,5 @@ export const BOARD_TYPE = {
   PUBLIC: 'public',
   PRIVATE: 'private'
 }
+
+export const WEBSITE_DOMAIN = (env.BUILD_MODE === 'production') ? env.WEBSITE_DOMAIN_PRODUCTION : env.WEBSITE_DOMAIN_DEVELOPMENT
